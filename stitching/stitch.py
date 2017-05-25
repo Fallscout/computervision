65bffa127743d5a3a92ec323cf0a700dbfb3a2ff
Fallscout <fallscout.dev@gmail.com>
2017-05-25 22:42:02 +0200

    Worked on image stitching
    Script works when using sift features, but not when using harris corners
    with patch features.

--- stitching/stitch.py
+++ stitching/stitch.py
@@ -5,31 +5,77 @@ def main():
     import argparse
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter,
         description="")
-    parser.add_argument("img1", type=str, help="")
-    parser.add_argument("img2", type=str, help="")
+    parser.add_argument("--left", default="img1.jpg", type=str, help="")
+    parser.add_argument("--right", default="img2.jpg", type=str, help="")
+    parser.add_argument("--use_sift", action="store_true")
     args = parser.parse_args()
     
-    img1 = cv2.imread(args.img1)
-    img2 = cv2.imread(args.img2)
+    img_left = cv2.imread(args.left)
+    img_right = cv2.imread(args.right)
 
-    output_image = im_stitch(img1, img2)
+    result = im_stitch(img_left, img_right, args.use_sift)
 
-def im_stitch(img1, img2):
-    gray_img1 = np.float32(cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY))
-    gray_img2 = np.float32(cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY))
+    return result
 
-    keypoints1 = detect_corners(gray_img1, 2, 3, 0.04, 0.01)
-    keypoints2 = detect_corners(gray_img2, 2, 3, 0.04, 0.01)
+def im_stitch(img_left, img_right, use_sift=False):
 
-    patches1 = extract_patches(gray_img1, keypoints1, 3, 3)
-    patches2 = extract_patches(gray_img2, keypoints2, 3, 3)
+    if use_sift:
+        gray_left = cv2.cvtColor(img_left, cv2.COLOR_BGR2GRAY)
+        gray_right = cv2.cvtColor(img_right, cv2.COLOR_BGR2GRAY)
 
-    distances = calculate_distances(patches1, patches2)
+        sift = cv2.xfeatures2d.SIFT_create()
+        (kps_left, features_left) = sift.detectAndCompute(gray_left, None)
+        (kps_right, features_right) = sift.detectAndCompute(gray_right, None)
+
+        kps_left = np.float32([kp.pt for kp in kps_left])
+        kps_right = np.float32([kp.pt for kp in kps_right])
+    else:
+        gray_left = np.float32(cv2.cvtColor(img_left, cv2.COLOR_BGR2GRAY))
+        gray_right = np.float32(cv2.cvtColor(img_right, cv2.COLOR_BGR2GRAY))
+
+        kps_left = detect_corners(gray_left, 2, 3, 0.04, 0.01)
+        kps_right = detect_corners(gray_right, 2, 3, 0.04, 0.01)
+
+        features_left = extract_patches(gray_left, kps_left, 3, 3)
+        features_right = extract_patches(gray_right, kps_right, 3, 3)
+    
+    print("Number of keypoints in left image: {}".format(len(kps_left)))
+    print("Left features shape: {}".format(features_left.shape))
+    print("Number of keypoints in right image: {}".format(len(kps_right)))
+    print("Right features shape: {}".format(features_right.shape))
+
+    distances = calculate_distances(features_right, features_left)
 
     corr_indices = np.argmin(distances, axis=1)
     matching_distances = np.min(distances, axis=1)
+    matching_keypoints = select_matches(kps_right, kps_left, corr_indices)
+    h, status = ransac(matching_keypoints)
 
-    matches = select_matches(patches1, patches2, corr_indices)
+    print(h)
+
+    result = cv2.warpPerspective(img_right, h, (img_left.shape[1] + img_right.shape[1], max(img_left.shape[0], img_right.shape[0])))
+    result[0:img_left.shape[0], 0:img_left.shape[1]] = img_left
+
+    return result
+
+def match_keypoints(kpsA, kpsB, featuresA, featuresB, ratio, reprojThresh):
+    matcher = cv2.DescriptorMatcher_create("BruteForce")
+    rawMatches = matcher.knnMatch(featuresA, featuresB, 2)
+    matches = []
+
+    for m in rawMatches:
+        if len(m) == 2 and m[0].distance < m[1].distance * ratio:
+            matches.append((m[0].trainIdx, m[0].queryIdx))
+
+    if len(matches) > 4:
+        ptsA = np.float32([kpsA[i] for (_, i) in matches])
+        ptsB = np.float32([kpsB[i] for (i, _) in matches])
+
+        (H, status) = cv2.findHomography(ptsA, ptsB, cv2.RANSAC, reprojThresh)
+
+        return (matches, H, status)
+
+    return None
 
 def select_matches(patches1, patches2, corr_indices):
     corr_points = []
@@ -54,7 +100,7 @@ def extract_patches(img, keypoints, width, height):
         row2 = keypoint[0] + border_height + 1
         col1 = keypoint[1] - border_width
         col2 = keypoint[1] + border_width + 1
-        patches.append(padded_image[row1:row2, col1:col2].flatten())
+        patches.append(padded_image[row1+border_height:row2+border_height, col1+border_width:col2+border_width].flatten())
 
     return np.array(patches)
 
@@ -67,11 +113,14 @@ def detect_corners(img, blockSize, ksize, k, thresh):
 
     return keypoints
 
-def ransac(distances, s, N, d, T):
-    #N = np.log(1-p)/np.log(1-(1-e)**s)
+def ransac(matches, s=None, N=None, d=None, T=None):
+    from sklearn import linear_model
+    base_points = np.array([match[0] for match in matches])
+    target_points = np.array([match[1] for match in matches])
 
-    for i in range(N):
-        sample = distances[np.random.randint(0, len(distances), s)]
+    h, status = cv2.findHomography(base_points, target_points, cv2.RANSAC)
+
+    return h, status
 
 if __name__ == "__main__":
-    main()
+    result = main()
